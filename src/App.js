6f3421bb76097c56d58c46f973cedc1f29b5ca18
NunesNathan <nunesxd7@hotmail.com>
2022-01-27 16:49:38 -0300

    :sparkles: (setup) - implementa rotas sugeridas

--- src/App.js
+++ src/App.js
@@ -1,21 +1,88 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './pages/Home';
+import Foods from './pages/Foods';
 
 function App() {
-  return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
+  const routes = () => (
+    <Switch>
+      <Route
+        path="/"
+        exact
       >
-        Glass
-      </object>
-    </div>
+        <Home />
+      </Route>
+      <Route
+        path="/foods"
+        exact
+      >
+        <Foods />
+      </Route>
+      <Route
+        path="/drinks"
+        exact
+      />
+      <Route
+        path="/foods/{id-da-receita}"
+      />
+      <Route
+        path="/drinks/{id-da-receita}"
+        exact
+      />
+      <Route
+        path="/foods/{id-da-receita}/in-progress"
+        exact
+      />
+      <Route
+        path="/drinks/{id-da-receita}/in-progress"
+        exact
+      />
+      <Route
+        path="/explore"
+        exact
+      />
+      <Route
+        path="/explore/foods"
+        exact
+      />
+      <Route
+        path="/explore/drinks"
+        exact
+      />
+      <Route
+        path="/explore/foods/ingredients"
+        exact
+      />
+      <Route
+        path="/explore/drinks/ingredients"
+        exact
+      />
+      <Route
+        path="/explore/foods/nationalities"
+        exact
+      />
+      <Route
+        path="/profile"
+        exact
+      />
+      <Route
+        path="/done-recipes"
+        exact
+      />
+      <Route
+        path="/favorite-recipes"
+        exact
+      />
+    </Switch>
   );
+  return (
+    <>
+      {
+        routes()
+      }
+    </>);
 }
 
 export default App;
