0303b7399186bdb18cc98bbc67ef017843f994c1
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-02-01 14:45:00 -0300

    :sparkles: (Explore) Implementado dois botões
    implementado os botoes de explorar drinks e explorar foods

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Button from '../components/Button';
 import Header from '../components/Header';
 
 export default function Explore() {
   return (
     <main>
       <Header title="Explorer" search={ false } />
+      <Button test="explore-foods" text="Explore Foods" />
+      <Button test="explore-drinks" text="Explore Drinks" />
     </main>
   );
 }
