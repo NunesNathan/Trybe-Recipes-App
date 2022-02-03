728e7fda288d8f6df82ccee0b7d94097ede3f544
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-02-02 21:38:43 -0300

    :recycle: (ReceitaInProgress) Corrigido a falta da quantidade de mistura
    Corrigido a falta da quantidade de mistura que a Renata percebeu que eu tinha me

--- src/components/RecipeIngredients.js
+++ src/components/RecipeIngredients.js
@@ -1,17 +1,57 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import RecipeIngredientsItem from './RecipeIngredientsItem';
 
-const RecipeIngredients = ({ recipe }) => (
-  <ul>
-    {Object.entries(recipe)
+const RecipeIngredients = ({ recipe }) => {
+  const [ingredients, SetIngredients] = useState([]);
+  const [measures, setMeasures] = useState([]);
+
+  /* const ingredientsAndMeasure = () => {
+    const arrIngredients = Object.entries(recipe)
       .filter((items) => items[0].includes('strIngredient'))
       .filter((items) => items[1])
-      .map((element, index) => (
-        <RecipeIngredientsItem key={ index } item={ element[1] } index={ index } />
-      ))}
-  </ul>
-);
+      .map((element) => element[1]);
+
+    SetIngredients(arrIngredients);
+
+    const arrMeasures = Object.entries(recipe)
+      .filter((items) => items[0].includes('strMeasure'))
+      .filter((items) => items[1])
+      .map((element) => element[1]);
+
+    setMeasures(arrMeasures);
+  }; */
+
+  useEffect(() => {
+    const arrIngredients = Object.entries(recipe)
+      .filter((items) => items[0].includes('strIngredient'))
+      .filter((items) => items[1])
+      .map((element) => element[1]);
+
+    SetIngredients(arrIngredients);
+
+    const arrMeasures = Object.entries(recipe)
+      .filter((items) => items[0].includes('strMeasure'))
+      .filter((items) => items[1])
+      .map((element) => element[1]);
+
+    setMeasures(arrMeasures);
+  }, [recipe]);
+
+  return (
+    <ul>
+      {ingredients && ingredients
+        .map((item, index) => (
+          <RecipeIngredientsItem
+            key={ index }
+            item={ item }
+            measure={ measures[index] }
+            index={ index }
+          />
+        ))}
+    </ul>
+  );
+};
 
 RecipeIngredients.propTypes = {
   recipe: PropTypes.object,
